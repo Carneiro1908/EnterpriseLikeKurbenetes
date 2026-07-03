42bf407af58970adce26e962dfbc36cfba0aa967
Carneiro1908 <tomasclima1406@gmail.com>
2026-07-03 15:57:59 +0100

    Fixing bootstrap workflow syntax

--- terraform/bootstrap/main_bootstrap/backend.tf
+++ terraform/bootstrap/main_bootstrap/backend.tf
@@ -1,13 +1,11 @@
 terraform {
     required_version = ">= 1.0.0"
 
-    #bucket         = "terraform-main-infra-bucket-${var.account_id}-${var.region}"
-    #    key            = "terraform.tfstate"
-    #    region         = var.region
-    #    dynamodb_table = "main_bootstrap_table"
-    #    encrypt       = true
-
     backend "s3" {
-        
+        bucket         = ""
+        key            = ""
+        region         = ""
+        dynamodb_table = "main_bootstrap_table" 
+        encrypt        = true
     }
 }
