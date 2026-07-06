7e5df18cd0fb01c786534f04a40b37c874129af0
Carneiro1908 <tomasclima1406@gmail.com>
2026-07-06 11:33:39 +0100

    Fixing the iamlive role syntax

--- terraform/bootstrap/microbootstrap/iam.tf
+++ terraform/bootstrap/microbootstrap/iam.tf
@@ -37,44 +37,8 @@ resource "aws_iam_role_policy_attachment" "admin_policy_attachment" {
 # attaching policies to terraform-bootstrap-role
 
 # Adding read only policy
-resource "aws_iam_policy" "readonly_policy" {
-    name = "terraform-iamlive-readonly-policy"
-    description = "Policy to allow iamlive read all resources and data from project to detect the policies needed"
 
-    policy = jsondecode({
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-            "Sid": "AllowReadAll",
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*"
-        },
-        {
-            "Sid": "BlockAnyModification",
-            "Effect": "Deny",
-            "Action": [
-                "*:Create*",
-                "*:Update*",
-                "*:Delete*",
-                "*:Put*",
-                "*:Tag*",
-                "*:Untag*",
-                "*:Modify*",
-                "*:Remove*",
-                "*:Change*",
-                "*:Attach*",
-                "*:Detach*",
-                "*:Associate*",
-                "*:Disassociate*"
-            ],
-            "Resource": "*"
-        }
-        ]
-    })   
-}
-
-resource "aws_iam_role_policy_attachment" "admin_policy_attachment" {
-    role = aws_iam_role.iamlive_role
-    policy_arn = aws_iam_policy.readonly_policy.arn
+resource "aws_iam_role_policy_attachment" "readonly_policy_attachment" {
+    role = aws_iam_role.iamlive_role.name
+    policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
