36b54fb9305359891a485fa79d46a458c6b29d27
Carneiro1908 <tomasclima1406@gmail.com>
2026-07-06 22:03:50 +0100

    Adding the bootstrap policy on micro bootstrap

--- terraform/bootstrap/microbootstrap/iam.tf
+++ terraform/bootstrap/microbootstrap/iam.tf
@@ -11,11 +11,82 @@ resource "aws_iam_role" "iamlive_role" {
 
 # attaching policies to terraform-bootstrap-role
 
-# Adding tempo administrator policy
+resource "aws_iam_policy" "bootstrap_policy" {
+  name        = "main-bootstrap-policy"
+  description = "Policy for main bootstrap"
 
-# attaching policies to terraform-bootstrap-role
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["sts:GetCallerIdentity", "iam:GetPolicy", "iam:GetPolicyVersion", "iam:ListPolicyVersions", "iam:DeletePolicy"]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:ListBucket"]
+        Resource = "arn:aws:s3:::terraform-main-bootstrap-bucket-eu-central-1"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["dynamodb:PutItem", "dynamodb:GetItem", "dynamodb:DeleteItem"]
+        Resource = "arn:aws:dynamodb:eu-central-1:547320736290:table/main_bootstrap_table"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:ListMultipartUploadParts", "s3:PutObject"]
+        Resource = "arn:aws:s3:::terraform-main-bootstrap-bucket-eu-central-1/enviroment/bootstrap/terraform.tfstate"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket", "s3:GetBucketPolicy", "s3:GetBucketAcl", "s3:GetBucketCORS",
+          "s3:GetBucketWebsite", "s3:GetBucketVersioning", "s3:GetAccelerateConfiguration",
+          "s3:GetBucketRequestPayment", "s3:GetBucketLogging", "s3:GetLifecycleConfiguration",
+          "s3:GetReplicationConfiguration", "s3:GetEncryptionConfiguration",
+          "s3:GetBucketObjectLockConfiguration", "s3:GetBucketPublicAccessBlock",
+          "s3:PutBucketVersioning", "s3:PutBucketPublicAccessBlock",
+          "s3:PutEncryptionConfiguration", "s3:DeleteBucket"
+        ]
+        Resource = "arn:aws:s3:::terraform-main-infra-bucket-eu-central-1"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["kms:DescribeKey", "kms:GetKeyPolicy", "kms:GetKeyRotationStatus", "kms:ListResourceTags", "kms:ScheduleKeyDeletion"]
+        Resource = "arn:aws:kms:eu-central-1:547320736290:key/0b38348b-a8c6-4817-9fb2-de18485c145f"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["iam:GetRole", "iam:ListRolePolicies", "iam:ListAttachedRolePolicies", "iam:DetachRolePolicy", "iam:ListInstanceProfilesForRole", "iam:DeleteRole"]
+        Resource = "arn:aws:iam::547320736290:role/*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["dynamodb:DescribeTable", "dynamodb:DescribeContinuousBackups", "dynamodb:DescribeTimeToLive", "dynamodb:DeleteTable"]
+        Resource = "arn:aws:dynamodb:eu-central-1:547320736290:table/main_infra_table"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["kms:DescribeKey"]
+        Resource = "arn:aws:kms:eu-central-1:547320736290:key/alias/aws/dynamodb"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:ListTagsForResource"]
+        Resource = "arn:aws:s3:eu-central-1:547320736290:access-grants/default"
+      }
+    ]
+  })
+}
 
-# Adding read only policy
+resource "aws_iam_role_policy_attachment" "bootstrap_policy_attachment" {
+    role = aws_iam_role.bootstrap_role.name  
+    policy_arn = aws_iam_policy.bootstrap_policy.arn
+}
+
+
+# Adding read only policy for debbuger 
 resource "aws_iam_role_policy_attachment" "readonly_policy_attachment" {
     role = aws_iam_role.iamlive_role.name
     policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
