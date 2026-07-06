f22d7fe8c71ab3da20f32622f5ef36d0e5809c99
Carneiro1908 <tomasclima1406@gmail.com>
2026-07-06 11:19:12 +0100

    Adding read only role  & Creating a policy detector workflow based on iamlive

--- terraform/bootstrap/microbootstrap/iam.tf
+++ terraform/bootstrap/microbootstrap/iam.tf
@@ -4,156 +4,12 @@ resource "aws_iam_role" "bootstrap_role" {
 
 }
 
-# attaching policies  
-
-# S3 policy
-
-resource "aws_iam_policy" "s3_policy" {
-    name = "terraform-bootstrap-s3-policy"
-    description = "Policy to allow access to S3 bucket for bootstrap"
-
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = [
-                    "s3:CreateBucket",
-                    "s3:DeleteBucket",
-
-                    "s3:GetObject",
-                    "s3:DeleteObject",
-                    "s3:ListBucket",
-                    "s3:PutObject",
-
-                    "s3:PutBucketPublicAccessBlock",
-                    "s3:PutBucketEncryption",
-                    "s3:PutBucketVersioning",
-                    "s3:GetBucketLocation",
-                    "s3:ListBucket",
-
-                    "s3:GetBucketPolicy",
-                ]
-                Effect   = "Allow"
-                Resource = ["*"]
-            }
-        ]
-    })
+resource "aws_iam_role" "iamlive_role" {
+    name = "iamlive-role"
+    assume_role_policy = data.aws_iam_policy_document.github_oidc_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
-    role       = aws_iam_role.bootstrap_role.name
-    policy_arn = aws_iam_policy.s3_policy.arn
-}
-
-# Dynamodb policy
-
-resource "aws_iam_policy" "dynamodb_policy" {
-    name = "terraform-bootstrap-dynamodb-policy"
-    description = "Policy to allow access to DynamoDB table for bootstrap"
-
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = [
-                    "dynamodb:CreateTable",
-                    "dynamodb:DeleteTable",
-                    "dynamodb:DescribeTable",
-                    "dynamodb:UpdateTable",
-                    "dynamodb:PutItem",
-                    "dynamodb:GetItem",
-                    "dynamodb:DeleteItem",
-                ]
-                Effect   = "Allow"
-                Resource = ["*"]
-            }
-        ]
-    })
-}
-
-resource "aws_iam_role_policy_attachment" "dynamodb_policy_attachment" {
-    role       = aws_iam_role.bootstrap_role.name
-    policy_arn = aws_iam_policy.dynamodb_policy.arn
-}
-
-# Iam actions policy
-
-resource "aws_iam_policy" "iam_actions_policy" {
-    name = "terraform-bootstrap-iam-actions-policy"
-    description = "Policy to allow access to IAM actions for bootstrap"
-
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = [
-                    # permissions with roles
-                    "iam:GetRole",
-                    "iam:CreateRole",
-                    "iam:DeleteRole",
-                    "iam:UpdateRole",          
-                    "iam:UpdateRoleDescription",
-
-                    # permissions with policies
-                    "iam:CreatePolicy",
-                    "iam:DeletePolicy",
-                    "iam:AttachRolePolicy",
-                    "iam:DetachRolePolicy",
-                    "iam:UpdateAssumeRolePolicy",
-                    "iam:GetPolicy",
-                    "iam:GetPolicyVersion",
-                    "iam:ListInstanceProfilesForRole",
-                    "iam:ListRolePolicies",
-                    "iam:ListAttachedRolePolicies"
-                ]
-                Effect   = "Allow"
-                Resource = ["*"]
-            }
-        ]
-    })
-}
-
-resource "aws_iam_role_policy_attachment" "iam_actions_policy_attachment" {
-    role       = aws_iam_role.bootstrap_role.name
-    policy_arn = aws_iam_policy.iam_actions_policy.arn
-}
-
-# KMS policy
-resource "aws_iam_policy" "kms_policy" {
-    name = "terraform-bootstrap-kms-policy"
-    description = "Policy to allow access to KMS for bootstrap"
-
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = [
-                    # permissions with keys
-                    "kms:CreateKey",
-                    "kms:DescribeKey",
-                    "kms:ScheduleKeyDeletion",
-                    "kms:CancelKeyDeletion",
-                    "kms:EnableKeyRotation",
-                    "kms:DisableKeyRotation",
-                    "kms:ListKeys",
-                    "kms:ListAliases",
-                    "kms:CreateAlias",
-                    "kms:Decrypt",
-                    "kms:GenerateDataKey*",
-                    "kms:DeleteAlias",
-                    "kms:GetKeyPolicy"
-                ]
-                Effect   = "Allow"
-                Resource = ["*"]
-            }
-        ]
-    })
-}
-
-resource "aws_iam_role_policy_attachment" "kms_policy_attachment" {
-    role       = aws_iam_role.bootstrap_role.name
-    policy_arn = aws_iam_policy.kms_policy.arn
-}
+# attaching policies to terraform-bootstrap-role
 
 # Adding tempo administrator policy
 resource "aws_iam_policy" "admin_policy" {
@@ -176,4 +32,49 @@ resource "aws_iam_policy" "admin_policy" {
 resource "aws_iam_role_policy_attachment" "admin_policy_attachment" {
     role       = aws_iam_role.bootstrap_role.name
     policy_arn = aws_iam_policy.admin_policy.arn
+}
+
+# attaching policies to terraform-bootstrap-role
+
+# Adding read only policy
+resource "aws_iam_policy" "readonly_policy" {
+    name = "terraform-iamlive-readonly-policy"
+    description = "Policy to allow iamlive read all resources and data from project to detect the policies needed"
+
+    policy = jsondecode({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+            "Sid": "AllowReadAll",
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "BlockAnyModification",
+            "Effect": "Deny",
+            "Action": [
+                "*:Create*",
+                "*:Update*",
+                "*:Delete*",
+                "*:Put*",
+                "*:Tag*",
+                "*:Untag*",
+                "*:Modify*",
+                "*:Remove*",
+                "*:Change*",
+                "*:Attach*",
+                "*:Detach*",
+                "*:Associate*",
+                "*:Disassociate*"
+            ],
+            "Resource": "*"
+        }
+        ]
+    })   
+}
+
+resource "aws_iam_role_policy_attachment" "admin_policy_attachment" {
+    role = aws_iam_role.iamlive_role
+    policy_arn = aws_iam_policy.readonly_policy.arn
 }
