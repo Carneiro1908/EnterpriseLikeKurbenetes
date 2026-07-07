5f64a87f5abf70a441306cc559de7fddcb9aa92e
Carneiro1908 <tomasclima1406@gmail.com>
2026-07-07 11:21:29 +0100

    Fixing main infra code

--- terraform/main_infra/output.tf
+++ terraform/main_infra/output.tf
@@ -1,11 +1,25 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
+# prod env vpc
+output "prod_env_vpc_id" {
+  value = module.prod_env_vpc.prod_env_vpc_id
 }
 
 output "private_subnet_ids" {
-  value = module.vpc.private_subnets
+  value = module.prod_env_vpc.private_subnets
 }
 
 output "public_subnet_ids" {
-  value = module.vpc.public_subnets
+  value = module.prod_env_vpc.public_subnets
+}
+
+# test env vpc
+output "test_env_vpc_id" {
+  value = module.test_env_vpc.test_env_vpc_id
+}
+
+output "private_subnet_ids" {
+  value = module.test_env_vpc.private_subnets
+}
+
+output "public_subnet_ids" {
+  value = module.test_env_vpc.public_subnets
 }
