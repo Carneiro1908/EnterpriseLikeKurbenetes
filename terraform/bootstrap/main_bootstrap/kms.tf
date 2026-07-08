2e549efda4040900b8aa995807a548a4238401bd
Carneiro1908 <tomasclima1406@gmail.com>
2026-07-08 12:05:23 +0100

    I will start infraestruture from scracth, because the old one was a chaos, and now it will be more organized

--- terraform/bootstrap/main_bootstrap/kms.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_kms_key" "dynamodb_key" {
-  description             = "KMS key for DynamoDB encryption"
-  deletion_window_in_days = 30
-  enable_key_rotation     = true # Boa prática de segurança
-}
