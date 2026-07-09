7e0270a87c595cd3c661f10481cc09b20717d1d6
Carneiro1908 <tomasclima1406@gmail.com>
2026-07-09 09:47:05 +0100

    Fixing workflows, adding new role for infraestructure debugger and correcting bootstrap

--- terraform/bootstrap/iam.tf
+++ terraform/bootstrap/iam.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_role" "infra_debugger_role" {
+    name = "infrastructure-debugger-role"
+    assume_role_policy = data.aws_iam_policy_document.github_actions_trust.json
+}
+
 resource "aws_iam_role" "infra_cicd_role" {
     name = "infrastructure-cicd-role"
     assume_role_policy = data.aws_iam_policy_document.github_actions_trust.json
@@ -8,4 +13,10 @@ resource "aws_iam_role" "infra_cicd_role" {
 resource "aws_iam_role_policy_attachment" "infra_cicd_role_attachment" {
     role = aws_iam_role.infra_cicd_role.name  
     policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" # Temporaly administrator
+}
+
+// In this case, the infra debugger role will always have administrator acsses, so it can debug infra without problems
+resource "aws_iam_role_policy_attachment" "infra_debugger_role_attachment" {
+    role = aws_iam_role.infra_debugger_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -3,14 +3,14 @@ output "github_actions_trust_policy" {
   value       = data.aws_iam_policy_document.github_actions_trust.json
 }
 
-output "state_bucket_name" {
+output "global_state_bucket_name" {
   value = module.state_bucket.bucket_id
 }
 
-output "state_lock_table_name" {
+output "global_state_lock_table_name" {
   value = module.state_lock_table.table_name
 }
 
-output "state_kms_key_arn" {
-  value = aws_kms_key.global_infra_kms_key.arn
+output "global_state_kms_key_arn" {
+  value = module.state_bucket.kms_key_arn
 }
