84f258c089bb48fd4069697cea09c23e942638aa
Carneiro1908 <tomasclima1406@gmail.com>
2026-07-10 18:42:37 +0100

    Fixing eks module and starting to develop a deploy workflow

--- terraform/infrastructure/modules/eks/main.tf
+++ terraform/infrastructure/modules/eks/main.tf
@@ -1,3 +1,4 @@
+# 1. THE EKS CLUSTER
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.31"
@@ -21,19 +22,11 @@ module "eks" {
 
   enable_irsa = var.enable_irsa
 
+  # Base addons that do not require IAM roles or do not cause dependency cycles
   cluster_addons = {
     vpc-cni    = { most_recent = true }
     coredns    = { most_recent = true }
     kube-proxy = { most_recent = true }
-    aws-ebs-csi-driver = {
-      most_recent              = true
-      service_account_role_arn = var.enable_irsa ? module.ebs_csi_irsa_role[0].iam_role_arn : null
-    }
-
-    amazon-cloudwatch-observability = var.enable_observability ? {
-      most_recent              = true
-      service_account_role_arn = module.cloudwatch_observability_irsa_role[0].iam_role_arn
-    } : null
   }
 
   eks_managed_node_groups = var.node_groups
@@ -43,6 +36,31 @@ module "eks" {
   })
 }
 
+# 2. IAM ROLES FOR ADDONS (IRSA)
+
+# IAM Role for EBS CSI Driver
+module "ebs_csi_irsa_role" {
+  count   = var.enable_irsa ? 1 : 0
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.44"
+
+  role_name = "${var.cluster_name}-ebs-csi-role"
+
+  role_policy_arns = {
+    policy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  }
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = var.tags
+}
+
+# IAM Role for CloudWatch Observability
 module "cloudwatch_observability_irsa_role" {
   count   = var.enable_observability ? 1 : 0
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
@@ -62,4 +80,28 @@ module "cloudwatch_observability_irsa_role" {
   }
 
   tags = var.tags
+}
+
+# 3. INDEPENDENT EKS ADDONS
+
+# EBS CSI Driver Addon
+resource "aws_eks_addon" "aws_ebs_csi_driver" {
+  count = var.enable_irsa ? 1 : 0
+
+  cluster_name = module.eks.cluster_name
+  addon_name   = "aws-ebs-csi-driver"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  service_account_role_arn    = module.ebs_csi_irsa_role[0].iam_role_arn
+}
+
+# CloudWatch Observability Addon
+resource "aws_eks_addon" "cloudwatch_observability" {
+  count = var.enable_observability ? 1 : 0
+
+  cluster_name = module.eks.cluster_name
+  addon_name   = "amazon-cloudwatch-observability"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  service_account_role_arn    = module.cloudwatch_observability_irsa_role[0].iam_role_arn
 }
